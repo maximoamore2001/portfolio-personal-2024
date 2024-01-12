6365b1ec15f6b0521d7bb5a4af889ef4eb55d77e
maximoamore2001 <maxiamorerc88@gmail.com>
2024-01-12 05:20:53 -0300

    cambios

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ const toggleText = document.getElementById('toggle-text');
 
 const toggleColors = document.getElementById('toggle-colors');
 
+const rootStyles = document.documentElement.style;
+
 toggleTheme.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     if(toggleIcon.src.includes('moon.svg')){
@@ -15,7 +17,7 @@ toggleTheme.addEventListener("click", () => {
     }
 })
 
-toggleColors.addEventListener('click', (e) => {
-    console.log(e.target.dataset);
+toggleColors.addEventListener("click", (e) => {
+    rootStyles.setProperty("--primary-color", e.target.dataset.color)
 }
 )
