27c25ab0114aaf381bdc46ad90293d909fcb1e91
maximoamore2001 <maxiamorerc88@gmail.com>
2024-01-15 14:38:00 -0300

    cambios

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,12 @@ const rootStyles = document.documentElement.style;
 
 const flagsElement = document.getElementById("flags");
 
-const changeLanguage = async language=>{
-    const requestJson = await fetch('./languages/${language}.json')
-    const texts = await requestJson.json()
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
 
     console.log(texts);
-}
+};
 
 flagsElement.addEventListener("click", (e) =>{
     changeLanguage(e.target.parentElement.dataset.language);
